e465fe94319584d81be5d1f6f7e66dedb2d88e28
Federico Ucchesu <federico.ucchesu@gmail.com>
2016-09-05 19:23:39 +0200

    fix for public server

--- progettoAmm/mvc/controller/dbSettings.php
+++ progettoAmm/mvc/controller/dbSettings.php
@@ -3,8 +3,8 @@
 class Settings {
 
     public static $db_host = 'localhost';
-    public static $db_user = 'root';
-    public static $db_password = 'davide';
+    public static $db_user = 'ucchesuFederico';
+    public static $db_password = 'lucertola4957';
     public static $db_name = 'amm15_ucchesuFederico';
 
 }
